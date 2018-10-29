305907002db1c2384bafd569939a9f4d7348a250
Tarc <Tarc@B87>
2018-10-29 11:11:44 +0800

    my second example

--- src/mynewgui/MyNewGUI.java
+++ src/mynewgui/MyNewGUI.java
@@ -16,6 +16,8 @@ public class MyNewGUI {
      */
     public static void main(String[] args) {
         System.out.println("Hello world 1");
+        
+        System.out.println("hello world 2");
     }
     
 }
